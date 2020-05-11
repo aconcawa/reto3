9bc60b36fdd974de5e1e3624a99041ce24b2c170
ivxn-bit <61226403+ivxn-bit@users.noreply.github.com>
2020-05-12 00:16:06 +0200

    Añadido control de rutas desde la aplicacion movil
    Añadido control de rutas desde la aplicacion movil.

--- codigocontrolmovil.ino
+++ codigocontrolmovil.ino
@@ -1,9 +1,44 @@
+#include <IRremote.h>
+
 int izqA = 5; 
 int izqB = 6; 
 int derA = 9; 
 int derB = 10; 
 int vel = 255; // Velocidad de los motores
 int estado = 'c';
+int RECV_PIN = 11;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void par() { //subrutina para parar el robot
+  analogWrite(derB, 0);     
+  analogWrite(izqB, 0); 
+  analogWrite(derA, 0);    
+  analogWrite(izqA, 0);
+}
+
+void ade() { //subrutina andar hacia delante
+  analogWrite(derB, 0);   
+  analogWrite(izqB, 0);
+  analogWrite(derA, vel);
+  analogWrite(izqA, vel);
+}
+
+void der() { //subrunita girar derecha
+  analogWrite(derB, 0);     
+  analogWrite(izqB, 0);
+  analogWrite(izqA, 0);
+  analogWrite(derA, vel);
+}
+
+void izq(){ //subrutina girar izquierda
+  analogWrite(derB, 0);     
+  analogWrite(izqB, 0); 
+  analogWrite(derA, 0);  
+  analogWrite(izqA, vel);
+}
 
 void setup()  { 
   pinMode(derA, OUTPUT);
@@ -11,12 +46,23 @@ void setup()  {
   pinMode(izqA, OUTPUT);
   pinMode(izqB, OUTPUT);
   Serial.begin(9600);
+  irrecv.enableIRIn();
  } 
  
 void loop()  { 
   if(Serial.available()>0){ //Busca cambios en el bluetooth, si los hay los guarda en la variable estado.
     estado = Serial.read();
   }
+  if (irrecv.decode(&results)) {
+  Serial.println(results.value, HEX);
+  irrecv.resume(); // Receive the next value
+  }
+  delay(100);
+  
+  if (results.value==0xFD00FF){ //Parado
+    par();
+  }
+  
   if(estado=='a'){
     analogWrite(derB, 0);     
     analogWrite(izqB, 0); 
@@ -47,5 +93,231 @@ void loop()  {
     analogWrite(izqA, 0);
     analogWrite(derB, vel);  
     analogWrite(izqB, vel);      
+  }
+  if(estado=='rut1'){
+    ade();
+    delay(60000);
+    der();
+    delay(300);
+    ade();
+    delay(58000);
+    der();
+    delay(300);
+    ade();
+    delay(36000);
+    izq();
+    delay(300);
+    ade();
+    delay(60000);
+    der();
+    delay(300);
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(53000);
+    izq();
+    delay(300);
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(77000);
+    der();
+    delay(300);
+    ade();
+    delay(55000);
+    par();
+  }
+  if(estado=='rut2'){
+    ade();
+    delay(58000);
+    der();
+    delay(300);
+    ade();
+    delay(36000);
+    izq();
+    delay(300);
+    ade();
+    delay(60000);
+    der();
+    delay(300);
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(53000);
+    izq();
+    delay(300);
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(77000);
+    der();
+    delay(300);
+    ade();
+    delay(55000);
+    par();
+  }
+  if(estado=='rut3'){
+    ade();
+    delay(36000);
+    izq();
+    delay(300);
+    ade();
+    delay(60000);
+    der();
+    delay(300);
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(53000);
+    izq();
+    delay(300);
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(77000);
+    der();
+    delay(300);
+    ade();
+    delay(55000);
+    par();
+  }
+  if(estado=='rut4'){
+    ade();
+    delay(60000);
+    der();
+    delay(300);
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(53000);
+    izq();
+    delay(300);
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(77000);
+    der();
+    delay(300);
+    ade();
+    delay(55000);
+    par();
+  }
+  if(estado=='rut5'){
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(53000);
+    izq();
+    delay(300);
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(77000);
+    der();
+    delay(300);
+    ade();
+    delay(55000);
+    par();
+  }
+  if(estado=='rut6'){
+    ade();
+    delay(53000);
+    izq();
+    delay(300);
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(77000);
+    der();
+    delay(300);
+    ade();
+    delay(55000);
+    par();
+  }
+  if(estado=='rut7'){
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(77000);
+    der();
+    delay(300);
+    ade();
+    delay(55000);
+    par();
+  }
+  if(estado=='rut8'){
+    ade();
+    delay(77000);
+    der();
+    delay(300);
+    ade();
+    delay(55000);
+    par();
+  }
+  if(estado=='rut9'){
+    ade();
+    delay(55000);
+    par();
+  }
+  if(estado=='rut10'){
+    ade();
+    delay(60000);
+    der();
+    delay(300);
+    ade();
+    delay(58000);
+    der();
+    delay(300);
+    ade();
+    delay(36000);
+    izq();
+    delay(300);
+    ade();
+    delay(60000);
+    der();
+    delay(300);
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(53000);
+    izq();
+    delay(300);
+    ade();
+    delay(39000);
+    der();
+    delay(300);
+    ade();
+    delay(77000);
+    der();
+    delay(300);
+    ade();
+    delay(55000);
+    par();
   }
 }
